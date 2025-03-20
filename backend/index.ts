6c97b64c6bf9154af4b551c6a0e70946f8586c50
EduardoAVS <eavsilva@sga.pucminas.br>
2025-03-20 14:59:37 -0300

    ReactGram iniciado em novo repositório

---
+++ backend/index.ts
@@ -0,0 +1,30 @@
+import "dotenv/config"; 
+import express from "express";
+import path from "path";
+import cors from "cors";
+
+import router from "./routes/Router";
+import conn from "./config/db"; // Importa a conexão com o banco de dados
+
+const port = process.env.PORT;
+
+const app = express();
+
+// Config JSON e form data response
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Solve CORS
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+
+// Upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// Rotas
+app.use(router);
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`);
+});
+
+conn();
